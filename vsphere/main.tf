ec616d102d1253c8228863e38d69c47671491125
Moff <jfrancisco.poo@gmail.com>
2020-08-25 20:18:39 -0500

    vSphere-v9

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -26,11 +26,17 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+data "vsphere_host" "host" {
+  name          = "hostip"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
   num_cpus = 4
+  host_system_id             = data.vsphere_host.host.id
   wait_for_guest_net_timeout = 0
   wait_for_guest_ip_timeout  = 0
   datacenter_id              = data.vsphere_datacenter.dc.id
