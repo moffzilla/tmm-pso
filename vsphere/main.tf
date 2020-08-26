6b3b3d7b1ed8ca0ba01ef19a76c672f33a15b56e
Moff <jfrancisco.poo@gmail.com>
2020-08-25 19:53:49 -0500

    vSphere-v5

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -37,6 +37,7 @@ resource "vsphere_virtual_machine" "vm" {
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
   num_cpus = 2
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
 
   network_interface {
     network_id = data.vsphere_network.network.id
