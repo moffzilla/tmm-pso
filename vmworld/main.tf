26ed5665393f8e00ac0547f32d926693ed85bc45
Moff <jfrancisco.poo@gmail.com>
2020-08-28 15:23:59 -0500

    Initial Draft

---
+++ vmworld/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    version = "2.69.0"
+    region="us-west-1"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "myTag" {
+  description = "My Input Tag"
+  default = "terraform-test"
+}
+
+resource "aws_instance" "machine1" {
+    ami           = "ami-0a63cd87767e10ed4"
+    instance_type = "t2.micro"
+    availability_zone = "us-west-1b"
+    tags = {
+      "type" = var.myTag
+    }
+}
