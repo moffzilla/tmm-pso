cdf9b1d237821132f331b861e6412a43a51628c4
Moff <jfrancisco.poo@gmail.com>
2020-09-01 17:50:43 -0500

    Dev - trigger pattern test08

--- vmworld/main.tf
+++ vmworld/main.tf
@@ -22,3 +22,12 @@ resource "aws_instance" "machine1" {
     }
 }
 
+resource "aws_instance" "machine2" {
+    ami           = "ami-0a63cd87767e10ed4"
+    instance_type = var.instance_type
+    availability_zone = "us-west-1c"
+    tags = {
+      "type" = var.myTag
+    }
+}
+
