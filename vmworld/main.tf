5b156f64b83fcac8dc68ff847a5b885ef25399e5
Moff <jfrancisco.poo@gmail.com>
2020-09-03 12:53:05 -0500

    Staging VMworld 1

--- vmworld/main.tf
+++ vmworld/main.tf
@@ -22,20 +22,8 @@ resource "aws_instance" "machine1" {
     }
 }
 
-resource "aws_instance" "machine2" {
-    ami           = "ami-0a63cd87767e10ed4"
-    instance_type = var.instance_type
-    availability_zone = "us-west-1b"
-    tags = {
-      "type" = var.myTag
-    }
-}
-
 output "instance_ips" {
   value = ["${aws_instance.machine1.*.public_ip}"]
 }
 
-output "instance_ips2" {
-  value = ["${aws_instance.machine2.*.public_ip}"]
-}
 
