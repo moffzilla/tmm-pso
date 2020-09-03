ccb98521f210d7f25af4216b7dc5f1168369c611
Moff <jfrancisco.poo@gmail.com>
2020-09-03 12:45:22 -0500

    Staging VMworld 100

--- vmworld/main.tf
+++ vmworld/main.tf
@@ -3,6 +3,16 @@ provider "aws" {
     region="us-west-1"
 }
 
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "myTag" {
+  description = "My Input Tag"
+  default = "terraform-test"
+}
+
 resource "aws_instance" "machine1" {
     ami           = "ami-0a63cd87767e10ed4"
     instance_type = var.instance_type
--- vmworld/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_type" {
-  description = "AWS instance type"
-  default     = "t2.micro"
-}
-
-variable "myTag" {
-  description = "My Input Tag"
-  default = "terraform-test"
-}
