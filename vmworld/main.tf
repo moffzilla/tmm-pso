0aadd61f6fc527f56ebeb029815b3957b0e32613
Moff <jfrancisco.poo@gmail.com>
2020-09-02 22:00:53 -0500

    vRAC TF Demo

--- vmworld/main.tf
+++ vmworld/main.tf
@@ -22,5 +22,14 @@ resource "aws_instance" "machine1" {
     }
 }
 
+resource "aws_instance" "machine2" {
+    ami           = "ami-0a63cd87767e10ed4"
+    instance_type = var.instance_type
+    availability_zone = "us-west-1b"
+    tags = {
+      "type" = var.myTag
+    }
+}
+
 
 
