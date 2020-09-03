300174e9223bce452ea7e55fe90fe190ed391101
Moff <jfrancisco.poo@gmail.com>
2020-09-03 11:51:21 -0500

    Draft VMworld 2 Node

--- vmworld/main.tf
+++ vmworld/main.tf
@@ -5,7 +5,16 @@ provider "aws" {
 
 resource "aws_instance" "machine1" {
     ami           = "ami-0a63cd87767e10ed4"
-    instance_type = "t2.micro"
+    instance_type = var.instance_type
+    availability_zone = "us-west-1b"
+    tags = {
+      "type" = var.myTag
+    }
+}
+
+resource "aws_instance" "machine2" {
+    ami           = "ami-0a63cd87767e10ed4"
+    instance_type = var.instance_type
     availability_zone = "us-west-1b"
     tags = {
       "type" = var.myTag
@@ -14,5 +23,6 @@ resource "aws_instance" "machine1" {
 
 output "instance_ips" {
   value = ["${aws_instance.machine1.*.public_ip}"]
+  value2 = ["${aws_instance.machine2.*.public_ip}"]
 }
 
