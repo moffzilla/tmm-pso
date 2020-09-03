dc7ff120728279b2c5b53774800946137b0a2a73
Moff <jfrancisco.poo@gmail.com>
2020-09-03 11:35:21 -0500

    Draft VMworld 1 Node

--- vmworld/main.tf
+++ vmworld/main.tf
@@ -3,16 +3,6 @@ provider "aws" {
     region="us-west-1"
 }
 
-variable "instance_type" {
-  description = "AWS instance type"
-  default     = "t2.micro"
-}
-
-variable "myTag" {
-  description = "My Input Tag"
-  default = "terraform-test"
-}
-
 resource "aws_instance" "machine1" {
     ami           = "ami-0a63cd87767e10ed4"
     instance_type = "t2.micro"
@@ -22,14 +12,7 @@ resource "aws_instance" "machine1" {
     }
 }
 
-resource "aws_instance" "machine2" {
-    ami           = "ami-0a63cd87767e10ed4"
-    instance_type = var.instance_type
-    availability_zone = "us-west-1c"
-    tags = {
-      "type" = var.myTag
-    }
+output "instance_ips" {
+  value = ["${aws_instance.machine1.*.public_ip}"]
 }
-
-
 
