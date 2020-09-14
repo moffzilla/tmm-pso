f695e5603ffc9a8008b6666541b9da306f49dec1
Moff <jfrancisco.poo@gmail.com>
2020-09-14 18:55:58 -0500

    vmc-draft

---
+++ vmc/main.tf
@@ -0,0 +1,28 @@
+provider vmc {
+  refresh_token = var.api_token
+  #org_id = var.org_id
+}
+
+data "vmc_connected_accounts" "my_accounts" {
+  #account_number = var.aws_account_number
+}
+
+resource "vmc_sddc" "sddc_1" {
+  sddc_name           = var.sddc_name
+  vpc_cidr            = var.vpc_cidr
+  num_host            = 3
+  provider_type       = "ZEROCLOUD"
+  region              = var.sddc_region
+  vxlan_subnet        = var.vxlan_subnet
+  delay_account_link  = false
+  skip_creating_vxlan = false
+
+  deployment_type     = "SingleAZ"
+
+  timeouts {
+    create = "300m"
+    update = "300m"
+    delete = "180m"
+  }
+
+}
