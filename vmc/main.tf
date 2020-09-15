bb13277a3c20ca5b304c1294d42923319c365e0f
Moff <jfrancisco.poo@gmail.com>
2020-09-15 18:08:46 -0500

    vmc-v4

--- vmc/main.tf
+++ vmc/main.tf
@@ -1,4 +1,4 @@
-provider vmc {
+provider "vmc" {
   refresh_token = var.api_token
   org_id = var.org_id
 }
@@ -7,22 +7,41 @@ data "vmc_connected_accounts" "my_accounts" {
   account_number = var.aws_account_number
 }
 
-resource "vmc_sddc" "sddc_1" {
+data "vmc_customer_subnets" "my_subnets" {
+  connected_account_id = data.vmc_connected_accounts.my_accounts.id
+  region               = replace(upper(var.sddc_region), "-", "_")
+}
+
+resource "vmc_sddc" "sddc_terra" {
   sddc_name           = var.sddc_name
   vpc_cidr            = var.vpc_cidr
-  num_host            = var.num_host
-  provider_type       = "ZEROCLOUD"
-  region              = var.sddc_region
+  num_host            = var.sddc_num_hosts
+  provider_type       = var.provider_type
+  region              = data.vmc_customer_subnets.my_subnets.region
   vxlan_subnet        = var.vxlan_subnet
   delay_account_link  = false
   skip_creating_vxlan = false
+  sso_domain          = "vmc.local"
+  deployment_type = "SingleAZ"
 
-  deployment_type     = "SingleAZ"
+  host_instance_type = var.host_instance_type
 
+#  storage_capacity = var.storage_capacity
+
+  account_link_sddc_config {
+    customer_subnet_ids  = [data.vmc_customer_subnets.my_subnets.ids[0]]
+    connected_account_id = data.vmc_connected_accounts.my_accounts.id
+  }
   timeouts {
     create = "300m"
     update = "300m"
     delete = "180m"
   }
-
 }
+
+resource "vmc_cluster" "cluster_1" {
+  sddc_id = vmc_sddc.sddc_terra.id
+  num_hosts = var.cluster_num_hosts
+}
+
+output "vc_url" { value = "${vmc_sddc.sddc_terra.vc_url}" }
--- vmc/variables.tf
+++ vmc/variables.tf
@@ -1,21 +1,60 @@
-variable api_token {
+variable "api_token" {
+  description = "API token used to authenticate when calling the VMware Cloud Services API."
+  default = ""
 }
-variable org_id {
+
+variable "org_id" {
+  description = "Organization Identifier."
+  default = ""
 }
-variable aws_account_number {
+
+variable "aws_account_number" {
+  description = "The AWS account number."
+  default     = ""
 }
-variable num_host {
-  default = "3"
+
+variable "sddc_name"{
+  description = "Name of SDDC."
+  default = "Terraform-SDDC-Bis"
 }
-variable sddc_name {    
-  default = "Terraform-SDDC"
+
+variable "sddc_region" {
+  description = "The AWS  or VMC specific region."
+  default     = "US_WEST_1"
 }
-variable sddc_region {
-  default = "US_WEST_1"
+
+variable "vpc_cidr" {
+  description = "SDDC management network CIDR. Only prefix of 16, 20 and 23 are supported."
+  default     = "172.31.48.0/20"
 }
-variable vpc_cidr {
-  default = "172.31.48.0/20"
+
+variable "vxlan_subnet" {
+  description = "A logical network segment that will be created with the SDDC under the compute gateway."
+  default     = "192.168.1.0/24"
 }
-variable vxlan_subnet {
-  default = "192.168.1.0/24"
+
+variable provider_type {
+  description = "Determines what additional properties are available based on cloud provider. Default value : AWS"
+  default     = "ZEROCLOUD"
 }
+
+variable host_instance_type {
+  description = "The instance type for the ESX hosts in the primary cluster of the SDDC. Possible values: I3_METAL, R5_METAL."
+  default     = "I3_METAL"
+}
+
+variable storage_capacity {
+  description = "The storage capacity value to be requested for the SDDC primary cluster. This variable is only for R5.METAL. Possible values are 15TB, 20TB, 25TB, 30TB, 35TB per host."
+  default     = "15TB"
+}
+
+variable cluster_num_hosts {
+  description = "The number of hosts in the cluster."
+  default = 3
+}
+
+variable sddc_num_hosts {
+  description = "The number of hosts in SDDC."
+  default     = 3
+}
+
