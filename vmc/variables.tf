0223f4570f04a1422ca93b140bfb1cc5bafba6a3
Moff <jfrancisco.poo@gmail.com>
2020-09-14 19:04:41 -0500

    vmc-v1

---
+++ vmc/variables.tf
@@ -0,0 +1,18 @@
+variable api_token {
+}
+variable org_id {
+}
+variable aws_account_number {
+}
+variable sddc_name {    
+  default = "Terraform-SDDC"
+}
+variable sddc_region {
+  default = "US_WEST_1"
+}
+variable vpc_cidr {
+  default = "172.31.48.0/20"
+}
+variable vxlan_subnet {
+  default = "192.168.1.0/24"
+}
